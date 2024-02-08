4ba9cfa0834e9cd592bd57685a8298c30e1c65c8
DINESH KUMAR K <117632461+dineshkuppuchamikkv@users.noreply.github.com>
2024-02-09 00:11:03 +0530

    Add files via upload

---
+++ src/users.js
@@ -0,0 +1,25 @@
+const users = [
+    {
+        img: "IMG_3737.jpg",
+        online: true,
+        name: "Dineshkumar K",
+        role: "FRONT END DEVELOPER",
+        skills: ["c", "java", "python"]
+    },
+    {
+        img: "20bit4023-K Dinesh.jpg",
+        online: true,
+        name: "DineshKumar K",
+        role: " BACK END DEVELOPER",
+        skills: ["c", "java", "python"]
+    },
+    {
+        img: "20221029191918_IMG_9517.JPG",
+        online: false,
+        name: "DineshKumar K",
+        role: "FRONT END DEVELOPER",
+        skills: ["c", "java", "python"]
+    }
+];
+
+export default users;
